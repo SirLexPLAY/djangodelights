634c375bb7a04de74622894c1eafca1468431acb
dawidjs <d.j.szefler@kjemi.uio.no>
2022-08-17 15:18:25 +0200

    Add Ingredient and MenuItem models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# This model represents an  ingredient that the restaurant has in its inventory.
+class Ingredient(models.Model):
+    POUNDS = "LB"
+    TABLESPOON = "TB"
+    TEASPOON = "TS"
+    CUPS = "CU"
+    UNIT_CHOICES = [
+        (POUNDS, "lbs"),
+        (TABLESPOON, "tbsp"),
+        (TEASPOON, "tsp"),
+        (CUPS, "c")
+    ]
+
+    name = models.CharField(max_length=30)
+    quantity = models.IntegerField(default=0)
+    unit = models.CharField(max_length=10, choices=UNIT_CHOICES)
+    unit_price = models.FloatField(default=0)
+
+# This model represents an item on the restaurant's menu.
+class MenuItem(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.FloatField(default=0)
+
