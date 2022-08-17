ad0c9a58da90fdc2204d65134fd10d17eae55c50
dawidjs <d.j.szefler@kjemi.uio.no>
2022-08-17 15:58:29 +0200

    Updated Ingredient. Added __str__ methods.

--- inventory/models.py
+++ inventory/models.py
@@ -3,14 +3,20 @@ from django.db import models
 # This model represents an  ingredient that the restaurant has in its inventory.
 class Ingredient(models.Model):
     POUNDS = "LB"
+    OUNCES = "OU"
+    GRAMS = "GR"
     TABLESPOON = "TB"
     TEASPOON = "TS"
     CUPS = "CU"
+    EGGS = "EG"
     UNIT_CHOICES = [
         (POUNDS, "lbs"),
+        (OUNCES, "ounces"),
+        (GRAMS, "grams"),
         (TABLESPOON, "tbsp"),
         (TEASPOON, "tsp"),
-        (CUPS, "c")
+        (CUPS, "c"),
+        (EGGS, "eggs")
     ]
 
     name = models.CharField(max_length=30)
@@ -18,10 +24,16 @@ class Ingredient(models.Model):
     unit = models.CharField(max_length=10, choices=UNIT_CHOICES)
     unit_price = models.FloatField(default=0)
 
+    def __str__(self):
+        return self.name
+
 # This model represents an item on the restaurant's menu.
 class MenuItem(models.Model):
     title = models.CharField(max_length=50)
     price = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.title
 
 # This model represents a single ingredient and how much of it is required for an item off the menu.
 class RecipeRequirement(models.Model):
@@ -29,7 +41,9 @@ class RecipeRequirement(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quanity = models.FloatField(default=0)
 
+
 # This model represents a customer purchase of an item off the menu.
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.DO_NOTHING)
     timestamp = models.DateTimeField()
+
