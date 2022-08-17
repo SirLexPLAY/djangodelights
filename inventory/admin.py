d44847f3269ed6072b94139521624ccfcb53192f
dawidjs <d.j.szefler@kjemi.uio.no>
2022-08-17 15:59:56 +0200

    Secrets :)

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 
+# admin login: dawid
+# admin password: admin123
+
 # Register your models here.
 admin.site.register(Ingredient)
 admin.site.register(MenuItem)
